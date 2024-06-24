e35285c12c98d7e433d811c5fedd0c614e884af2
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-06-24 18:25:00 +0200

    put

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,14 @@
-  const express = require('express')
-  const contacts = require('../../contacts.json')
-  const { v4: uuidv4 } = require('uuid');
+const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs').promises;
-
-  const router = express.Router()
+const router = express.Router()
 
 router.get('/', async (req, res, next) => {
   try {
     const data = await fs.readFile('./contacts.json', 'utf8');
     res
     .status(200)
-    .json(JSON.parse(data));
+    .send(JSON.parse(data));
   } catch (err) {
     console.error(err);
     next(err);
@@ -30,7 +28,7 @@ router.get('/:contactId', async (req, res, next) => {
     } else {
       res
         .status(200)
-        .send(`id ${item.id} \n name: ${item.name} \n email: ${item.email} \n phone: ${item.phone}`)
+        .send(`id: ${item.id} \t name: ${item.name} \t email: ${item.email} \t phone: ${item.phone}`)
     }
   } catch (err) {
     console.error(err);
@@ -39,12 +37,23 @@ router.get('/:contactId', async (req, res, next) => {
 
 
 router.post('/', async (req, res, next) => {
-  const { namee, email, phone } =req.body
+  const { name, email, phone } =req.body
   const user= {
-       "id": uuidv4(),
-          "name": namee,
+    "id": uuidv4(),
+          "name": name,
           "email": email,
           "phone": phone
+  }
+  try {
+    const data = await fs.readFile('./contacts.json', 'utf8');
+    const items = JSON.parse(data)
+    items.push(user)
+    await fs.writeFile('./contacts.json', JSON.stringify(items, null, 2), 'utf8');
+    res.status(201)
+    .json(`contact id = ${user.id} was created`)
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
 })
 
@@ -74,7 +83,32 @@ router.delete('/:contactId', async (req, res, next) => {
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const { name, email, phone } = req.body
+
+  try {
+    const data = await fs.readFile('./contacts.json', 'utf8');
+    const items = JSON.parse(data)
+    const item = items.find(contact => contact.id == contactId)
+    const index = items.findIndex(contact=>contact.id == contactId)
+
+    if (item) {
+       contact = {
+        "id": contactId,
+         "name": name || item.name,
+          "email": email || item.email,
+          "phone": phone || item.phone
+      }
+      items[index]=contact
+    }
+    await fs.writeFile('./contacts.json', JSON.stringify(items, null, 2), 'utf8');
+    res.status(201)
+    .json(`contact id = ${contactId} was updated`)
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+  
 })
 
 module.exports = router
